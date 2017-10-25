ca5e401c7c01b53b019514d0042efa0405761695
mryozer <ryan@bw-family.com>
2017-10-25 10:05:20 -0700

    added network connection check to main screen

--- app/src/main/java/com/android/lazertag/MainMenu.java
+++ app/src/main/java/com/android/lazertag/MainMenu.java
@@ -1,15 +1,26 @@
 package com.android.lazertag;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
+    ConnectivityManager connMgr;
+    NetworkInfo netInfo;
+    boolean connection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        netInfo = connMgr.getActiveNetworkInfo();
+        connection = netInfo.isConnected();
     }
 
     public void goToJoin(View view){
