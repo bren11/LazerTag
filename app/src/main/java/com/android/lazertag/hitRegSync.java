dec961b537c00fcf1403c0a782a1dd43a0ab5d65
mryozer <ryan@bw-family.com>
2017-11-15 10:03:50 -0800

    fixed the volley

--- app/src/main/java/com/android/lazertag/hitRegSync.java
+++ app/src/main/java/com/android/lazertag/hitRegSync.java
@@ -1,25 +1,84 @@
 package com.android.lazertag;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import android.util.Log;
 
-import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by meme on 10/25/2017.
+ * Created by Ryan Whiting on 10/25/2017.
  */
 
-class hitRegSync /*implements AsyncTask<URL, Integer, Long> */{
-/*
-    public hitRegSync(){
+class HitRegSync {
 
+    private Context context;
+    RequestQueue queue;
+    private String response;
+
+    public HitRegSync(Context context) {
+        this.context = context;
+        queue = Volley.newRequestQueue(this.context);
     }
 
-    public Long doInBackground(URL url){
+    public void networkPost(final String data, String urlTarget){
+        StringRequest postRequest = new StringRequest(Request.Method.POST, urlTarget,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        HitRegSync.this.response = response;
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("data", data);
 
-        return
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 
-    public void onPostExecute(){
+    public void networkGet(String urlTarget) {
+        StringRequest getRequest = new StringRequest(Request.Method.GET, urlTarget,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // display response
+                        HitRegSync.this.response = response;
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+        queue.add(getRequest);
+    }
 
-    }*/
+
 }
--- openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\dphip\\StudioProjects\\LazerTag\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\meme\\Documents\\GitHub\\LazerTag\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
